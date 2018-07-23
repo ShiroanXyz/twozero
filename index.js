dc58f6902816dc8fc6c1f8c2df8f403660bc5921
ShiroanXyz <40276172+ShiroanXyz@users.noreply.github.com>
2018-07-23 08:37:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', msg => {
 });
 
 client.on('guildMemberAdd', (member) => {
-    member.sendMessage('**Hello! ♡**\nWould you be kind enough to join my server?\nHere is the link: **https://discord.gg/TaN42wa**', {
+    member.sendMessage('**Hello! ♡**\nWould you be kind enough to join my server?\nHere is the link: **https://discord.gg/xsHxkpf**', {
       embed: {
         color: 0xd10000,
         image: {
